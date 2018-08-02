16978f53f47fcb6af886cb8a7ba360b46c9d8f89
Uday Trivedi <trivediu@gmail.com>
2018-08-02 12:52:58 -0700

    Implement Dashboard in Navbar

---
+++ client/src/components/dashboard/dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+/**
+* A component to hold the user dashboard
+*/
+
+var DashBoard = props => (
+  <div>
+    Dashboard Test
+  </div>
+);
+
+export default DashBoard;
